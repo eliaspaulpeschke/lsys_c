882ab4bc1f1de99806f778b43b41005925181619
Elias Peschke <eliaspeschke@googlemail.com>
2026-01-30 17:03:53 +0100

    idk

--- main.c
+++ main.c
@@ -1,160 +1,45 @@
 
 #include "raylib.h"
 #include "raymath.h"
-#include <math.h>
+//#include <math.h>
 #include <stdbool.h>
+/*#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "lib/lsystem.h"
 #include "lib/lsystem_parser.h"
-#include "lib/turtle.h"
+#include "lib/turtle.h"*/
 
-Color getCol(int i){
-    switch (i){
-        case 1:
-            return WHITE;
-        case 2:
-            return RED;
-        case 3: 
-            return GREEN;
-        case 4:
-            return BLUE;
-        default:
-            return YELLOW;
-    }
-}
-
-void test(char * str){
-    int open = 0;
-    for(int i = 0; i < strlen(str); i++){
-        switch (str[i]) {
-            case '[':
-                open++;
-                break;
-            case ']':
-                open--;
-                break;
-            default:
-                break;
-        }
-        if (open < 0) printf("unbalanced: %d of %lu \n",i, strlen(str));
-    }
-}
-
+#include "ui.c"
 
 int main(void)
 {
+    SetTraceLogLevel(LOG_ALL);
+    const int screenWidth = 1280;
+    const int screenHeight = 720;
 
-    Lsystem sys = lsystem_from_file("test", true);
-
-    char * res = applyRules(sys.ruleset, sys.nrules, sys.axiom, false);
-    res = applyRules(sys.ruleset, sys.nrules, res, true);
-    res = applyRules(sys.ruleset, sys.nrules, res, true);
-    res = applyRules(sys.ruleset, sys.nrules, res, true);
-
-    for (int i = 0; i < sys.nrules; i++){
-        print_rule(&sys.ruleset[i], "  ");
-    }
-
-    printf("string length: %lu \n", strlen(res));
-
-    test(res);
-
-    printf("\n\"%s\"\n", res);
-
-    const int screenWidth = 600;
-    const int screenHeight = 600;
-
-    int col = 1;
-
-    SetWindowState(FLAG_WINDOW_HIGHDPI | FLAG_VSYNC_HINT);
-
-    Camera2D camera = {0};
-    camera.target = (Vector2){0.0f, 0.0f};
-    camera.offset = (Vector2){0.0f, 0.0f};
-    camera.rotation = 1.0f;
-    camera.zoom = 1.0f;
-
-    InitWindow(screenWidth, screenHeight, "fun");
+//    InitWindow(screenWidth, screenHeight, "testing");
+    Clay_Raylib_Initialize(screenWidth, screenHeight, "Aristid", FLAG_WINDOW_HIGHDPI | FLAG_VSYNC_HINT);
+//
     SetTargetFPS(60);
+    
+    clay_ctx clay_context = init_clay();
+    Font fonts[1];
 
-    Turtle * turtle = malloc(sizeof(Turtle));
-    *turtle = (Turtle){ (Vector2){300.0f, 300.0f}
-                , (Vector2){10.0f, 0.0f}
-                , 1.0f
-                , PI / 3.0f };    
+    fonts[0]  = LoadFontEx("resources/fonts/roboto_mono/static/RobotoMono-Bold.ttf",48,NULL,0);
+    SetTextureFilter(fonts[0].texture, TEXTURE_FILTER_BILINEAR);
 
+    Clay_SetMeasureTextFunction(Raylib_MeasureText, fonts); 
     while (!WindowShouldClose())    
     {
-        float delta = 1.5f;
-        float delta_zoom = 1.05f;
-
-        if (IsKeyDown(KEY_LEFT)) {
-                camera.target.x = camera.target.x + delta;
-        }
-        if (IsKeyDown(KEY_RIGHT)) {
-                camera.target.x = camera.target.x - delta;
-        }
-        if (IsKeyDown(KEY_UP)) {
-                camera.target.y = camera.target.y + delta;
-        }
-        if (IsKeyDown(KEY_DOWN)) {
-                camera.target.y = camera.target.y - delta;
-        }
-        if (IsKeyDown(KEY_P)) {
-                camera.zoom *= delta_zoom;
-        }
-        if (IsKeyDown(KEY_M)) {
-                camera.zoom /= delta_zoom;
-        }
-        if (IsKeyDown(KEY_W)) {
-            turtle->rads += 0.1f;
-        }
-        if (IsKeyDown(KEY_Q)) {
-            turtle->rads -= 0.1f;
-        }
-
-        turtle->pos = (Vector2){300.0f, 300.0f};
-        turtle->heading = (Vector2){10.0f, 0.0f};
-
+        Clay_RenderCommandArray layout = mk_layout(clay_context);
         BeginDrawing();
         ClearBackground(BLACK);
-        BeginMode2D(camera);
-
-        for (uint i = 0; i < strlen(res); i++) {
-            switch (res[i]) {
-                case 'F':
-                    moveTurtleCol(turtle, getCol(col));
-                    break;
-                case '+':
-                    rotateTurtle(turtle, true);
-                    break;
-                case '-':
-                    rotateTurtle(turtle, false);
-                    break;
-                case '[':
-                    col +=1;
-                    turtle = pushTurtle(turtle);
-                    break;
-                case ']':
-                    col -=1;
-                    turtle = popTurtle(turtle);
-                    break;
-                default:
-                    break;
-            }
-        } 
-        EndMode2D();
+        Clay_Raylib_Render(layout, fonts);
         EndDrawing();
     }
-    free_lsystem(sys);
-    while (turtle->prev != NULL) {
-        turtle = popTurtle(turtle);
-    }
-    free(turtle);
-    free(res);
-    CloseWindow();   
+    Clay_Raylib_Close();
     return 0; 
 }
 
---
+++ ui.c
@@ -0,0 +1,109 @@
+
+
+#include "raylib.h"
+#include <stdio.h>
+#define CLAY_IMPLEMENTATION
+#include "lib/clay.h"
+#include "lib/clay_renderer_raylib.c"
+
+typedef struct {
+    Clay_Context * ctx;
+} clay_ctx;
+
+
+const Clay_Color CLAY_LIGHT = (Clay_Color) {224, 215, 210, 255};
+const Clay_Color CLAY_RED = (Clay_Color) {168, 66, 28, 255};
+const Clay_Color CLAY_ORANGE = (Clay_Color) {225, 138, 50, 255};
+
+
+void HandleClayErrs(Clay_ErrorData errorData) {
+        printf("CLAY ERROR: %s \n", errorData.errorText.chars);
+}
+
+clay_ctx init_clay(){
+    uint64_t clayReqMem = Clay_MinMemorySize();
+    Clay_Arena clayMem = 
+        Clay_CreateArenaWithCapacityAndMemory(clayReqMem, malloc(clayReqMem));
+
+    Clay_Context *ctx = Clay_Initialize(clayMem
+            , (Clay_Dimensions) {
+                  .width = GetScreenWidth()
+                , .height = GetScreenHeight() 
+                }
+            , (Clay_ErrorHandler) {HandleClayErrs} 
+            );
+
+
+    return (clay_ctx){
+          .ctx = ctx
+    };
+}
+
+// Layout config is just a struct that can be declared statically, or inline
+Clay_ElementDeclaration sidebarItemConfig = (Clay_ElementDeclaration) {
+    .layout = {
+        .sizing = { .width = CLAY_SIZING_GROW(0), .height = CLAY_SIZING_FIXED(50) }
+    },
+    .backgroundColor = CLAY_ORANGE
+};
+
+// Re-useable components are just normal functions
+void SidebarItemComponent(Clay_ElementId id) {
+    CLAY(id, sidebarItemConfig) {
+        // children go here...
+    }
+}
+
+Clay_RenderCommandArray mk_layout(clay_ctx ctx){
+    Clay_SetCurrentContext(ctx.ctx);
+    Clay_SetDebugModeEnabled(true);
+    Vector2 mouse = GetMousePosition();
+    Clay_SetPointerState( (Clay_Vector2) {mouse.x, mouse.y}
+                        , IsMouseButtonDown(0)
+                        );
+    Vector2 scroll = GetMouseWheelMoveV();
+    Clay_UpdateScrollContainers( true
+                               , (Clay_Vector2) {scroll.x, scroll.y}
+                               , GetFrameTime()
+                               );
+    Clay_SetLayoutDimensions((Clay_Dimensions){.width = GetScreenWidth(),
+                                               .height = GetScreenHeight()});
+    Clay_BeginLayout();
+    
+    CLAY(CLAY_ID("OuterContainer")
+        , { .layout = { .sizing = {CLAY_SIZING_GROW(0), CLAY_SIZING_GROW(0)}
+                      , .padding = CLAY_PADDING_ALL(16)
+                      , .childGap = 16 }
+        , .backgroundColor = {0,0,0,0} }) {
+          
+          CLAY(CLAY_ID("SideBar")
+               , { .layout = { .layoutDirection = CLAY_TOP_TO_BOTTOM
+                             , .sizing = { .width = CLAY_SIZING_FIXED(300)
+                             , .height = CLAY_SIZING_GROW(0) }
+                             , .padding = CLAY_PADDING_ALL(16)
+                             , .childGap = 16 }
+                 , .backgroundColor = CLAY_LIGHT }) {
+            
+                   CLAY(CLAY_ID("ProfilePictureOuter")
+                       , { .layout = { .sizing = { .width = CLAY_SIZING_GROW(0) }
+                                     , .padding = CLAY_PADDING_ALL(16)
+                                     , .childGap = 16
+                                     , .childAlignment = { .y = CLAY_ALIGN_Y_CENTER } }
+                        , .backgroundColor = CLAY_RED }) {
+                        
+                          CLAY(CLAY_ID("ProfilePicture"), {.layout = { .sizing = { .width = CLAY_SIZING_FIXED(60), .height = CLAY_SIZING_FIXED(60) }} }) {}
+                          CLAY_TEXT( CLAY_STRING("Clay - UI Library")
+                                   , CLAY_TEXT_CONFIG({ .fontSize = 16, .fontId = 0, .textColor = {255, 255, 255, 255} }));
+                      }
+
+                   for (int i = 0; i < 5; i++) {
+                       SidebarItemComponent(CLAY_IDI("hi", i));
+                   }
+
+                   CLAY(CLAY_ID("MainContent"), { .layout = { .sizing = { .width = CLAY_SIZING_GROW(0), .height = CLAY_SIZING_GROW(0) } }, .backgroundColor = CLAY_LIGHT}) {}
+            }
+        }
+    return Clay_EndLayout();
+}
+
+
